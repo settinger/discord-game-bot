79697a875da23fd0a0811c451318f6623d5a6293
Sam Ettinger <ettingersam@gmail.com>
2021-02-18 14:44:23 +0100

    Fixed error in  command

--- games/config.py
+++ games/config.py
@@ -16,4 +16,4 @@ def game_details():
   if currGame == 'none':
     return "No game is active at the moment. Pick a game with `$setgame {gamename}.`\n" + available_games()
   else:
-    return f"Active game is **{currGame}**." + "\n\n" + loadedGames[currGame][intro] + "\n\nOther available bot commands:\n`$setgame {gamename}` -- Changes the active game.\n`$help` -- shows this page."
+    return f"Active game is **{currGame}**." + "\n\n" + loadedGames[currGame]['intro'] + "\n\nOther available bot commands:\n`$setgame {gamename}` -- Changes the active game.\n`$help` -- shows this page."
